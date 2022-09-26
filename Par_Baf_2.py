1a9ec74c92fb72b7f7a4d00f6bd9cce76767ab18
grishaev <oleg-g@yandex.ru>
2022-09-26 18:00:23 +0300

    Initial commit

--- Par_Baf_2.py
+++ Par_Baf_2.py
@@ -11,23 +11,30 @@ browser = webdriver.Chrome()
 browser.maximize_window()
 
 # Открываем сайт
-browser.get("https://hh.ru/")
+browser.get("https://www.bafus.ru/200100782/") #открываем страницу Евролайф 1л база А
 
-# Находим поле ввода текста и вводим текст
-search_input = browser.find_element(By.ID, "a11y-search-input")
-search_input.send_keys("Python Junior")
+# # Находим поле ввода текста и вводим текст
+# search_input = browser.find_element(By.ID, "a11y-search-input")
+# search_input.send_keys("Python Junior")
 
-# Находим кнопку "Найти работу"
-search_button = browser.find_element(By.CSS_SELECTOR, 'button[data-qa="search-button"]')
-search_button.click()
-
-# Найдем заголовок
-header_vacancies = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"]')
+# Найдем цену на 1л
+header_vacancies = browser.find_element(By.CSS_SELECTOR, '[span.data-orig-price.class]')
 header_name = header_vacancies.text
 
+
+# # Находим кнопку "Найти работу"
+# search_button = browser.find_element(By.CSS_SELECTOR, 'button[data-qa="search-button"]')
+# search_button.click()
+
+# # Найдем заголовок
+# header_vacancies = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"]')
+# header_name = header_vacancies.text
+
 # Форматируем строку с помощью регулярного выражения
-vacancies_count = re.sub(r"\D", "", header_name)
-print(f"Вакансий: {vacancies_count}")
+vacancies_count = header_name
+# vacancies_count = re.sub(r"\D", "", header_name)
+print(f"Цена: {vacancies_count}")
+print(header_vacancies)
 
 # Ждем
 time.sleep(2)
