67b44836ff67f75cc0104c38f7dfbd4c1f1895ab
grishaev <oleg-g@yandex.ru>
2022-10-20 18:01:41 +0300

    Initial commit

---
+++ ССЫЛКИ.py
@@ -0,0 +1,5 @@
+поиск строки эксель по индексу
+https://translated.turbopages.org/proxy_u/en-ru.ru.d6d15deb-63515ff9-54b285c3-74722d776562/https/stackoverflow.com/questions/51800122/using-openpyxl-to-find-rows-that-contain-cell-with-specific-value
+
+pandas
+http://pandas.geekwriter.ru/getting_started/index.html
