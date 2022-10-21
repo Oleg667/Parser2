b867097b272f20147eb7e6c9845bbdc35b33f405
grishaev <oleg-g@yandex.ru>
2022-10-21 13:55:58 +0300

    Initial commit

--- Par_Baf_2.py
+++ Par_Baf_2.py
@@ -1,14 +1,15 @@
-import re
 import time
 import chromedriver_binary
 import openpyxl
 import time
+import re
+
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
+from openpyxl.utils import get_column_letter
 
 #  Создаем браузер
 browser = webdriver.Chrome()
@@ -23,14 +24,12 @@ browser.maximize_window()
 # - локатор наименования лкм
 # - локатор тары
 # - локатор базы
+
 def open_get(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza): #открываем страницу по адресу и вытаскивает цену
 
+        browser.get(url_1) #открытие страницы по переданному адресу
 
-
-        browser.get(url_1) #jnrhsnbt страницы по переданному адресу
-
-
-        time.sleep(3)
+        time.sleep(0.4)
 
         err='Цена' # err - переменная показывающая какой этап парсинга выполняется, для идентификация ошибки
         price_n=' '
@@ -67,77 +66,89 @@ def open_get(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza): #отк
                 pass
         # browser.close() #закрываем браузер
 
-        time.sleep(3)
+        # time.sleep(3)
 
 
         return(name_n,tara_n,baza_n,price_n)
 
+
+def price_rrc(cod_sku,sheet_active): #получаем РРЦ из прайс-листа по коду товара
+
+        path_to_file = 'PRICE2022.xlsx'
+
+        # search_text = input(str('Какой текст ищем: '))
+        # search_text = search_text.lower()
+        search_text = cod_sku
+        # print('Ищем:', search_text)
+
+        wb = openpyxl.load_workbook(path_to_file)  # Грузим наш прайс-лист
+
+        sheets_list = wb.sheetnames  # Получаем список всех листов в файле
+        #sheet_active = wb[sheets_list[0]]  # Начинаем работать с самым первым
+        row_max = sheet_active.max_row  # Получаем количество столбцов
+        # print(type(row_max))
+        column_max = sheet_active.max_column  # Получаем количество строк
+
+        # print('В файле:', path_to_file, '\n Строк:', row_max, '\n Столбцов:', column_max)
+
+        row_min = 1  # Переменная, отвечающая за номер строки
+        column_min = 1  # Переменная, отвечающая за номер столбца
+
+        while column_min <= column_max:
+                row_min_min = row_min
+                row_max_max = row_max
+                while row_min_min <= row_max_max:
+                        row_min_min = str(row_min_min)
+
+                        word_column = get_column_letter(column_min)
+                        word_column = str(word_column)
+                        word_cell = word_column + row_min_min
+
+                        data_from_cell = sheet_active[word_cell].value
+                        data_from_cell = str(data_from_cell)
+                        # print(data_from_cell)
+                        regular = search_text
+                        result = re.findall(regular, data_from_cell)
+                        if len(result) > 0:
+                                # print('Нашли в ячейке:', word_cell)
+                                RRC = 'C' + row_min_min
+                                RRC_SKU = sheet_active[RRC].value # переменная хранит найденное РРЦ
+                                # print('РРЦ равняется ', RRC_SKU, ' рубль')
+                        row_min_min = int(row_min_min)
+                        row_min_min = row_min_min + 1
+                column_min = column_min + 1
+        return(RRC_SKU)
+
 wb = openpyxl.reader.excel.load_workbook(filename="PRICE2022.xlsx", data_only=True) #открываем файл с переменными
 wb.active = 1 # делаем активной вторую страницу  там где Бафус
-sheet = wb.active # копируем страницу в переменную
+sheet_parser = wb.active # копируем страницу в переменную
+wb.active = 0 # делаем активной первую страницу с прайсом РРЦ
+sheet_active = wb.active
+
 i=2
 while i>0:
 
         # извлекаем данные для парсинга
-        cod_sku = sheet['a'+ str(i)].value
-        url_1= sheet['b'+ str(i)].value
-        xpath_prise = sheet['c'+ str(i)].value
-        xpath_lkm = sheet['d'+ str(i)].value
-        xpath_tara = sheet['e'+ str(i)].value
-        xpath_baza = sheet['f'+ str(i)].value
+        cod_sku = sheet_parser['a'+ str(i)].value
+        url_1= sheet_parser['b'+ str(i)].value
+        xpath_prise = sheet_parser['c'+ str(i)].value
+        xpath_lkm = sheet_parser['d'+ str(i)].value
+        xpath_tara = sheet_parser['e'+ str(i)].value
+        xpath_baza = sheet_parser['f'+ str(i)].value
 
-#print(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza) #контрольная  печать переменных
+        # print(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza) #контрольная  печать переменных
 
-# url_1="https://www.bafus.ru/200100780/"
-# xpath_prise = '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[2]/aside/div/div[1]/table[1]/tbody/tr/td/span[1]'
-# xpath_lkm = '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/header/div/div/div/h1'
-# xpath_tara = '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[1]/div/div/div/section/div/ul/li[1]/div[2]/div/div/div[2]/div/span'
-# xpath_baza = '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[1]/div/div/div/section/div/ul/li[2]/div[2]/div/div/div[1]/div/span'
+
         if cod_sku == "stop":
                 browser.close()  # закрываем браузер
                 break
-        print(open_get(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza)) # получаем данные по продукту с сайта
+        # print(open_get(cod_sku, url_1, xpath_prise, xpath_lkm, xpath_tara, xpath_baza))
+        parser = open_get(cod_sku,url_1,xpath_prise,xpath_lkm,xpath_tara,xpath_baza) # получаем данные по продукту с сайта
+        Price_parser = parser[3]
+        Lkm = parser[0]
+        Tara = parser[1]
+        Baza = parser[2]
+        Sku_rrc = price_rrc(cod_sku,sheet_active)
+        print(Lkm,Tara,Baza,'цена на сайте',Price_parser,'РРЦ равно ',Sku_rrc)
         i = i+1
 
-#print(yyy)
-#print(name_n, tara_n, price_n)
-
-# Найдем цену на 1л
-# price = browser.find_elements(By.XPATH, '//span[@class="sht-price"][1]')
-
-
-# # Форматируем строку с помощью регулярного выражения
-# print(f"Цена за 1 л: {price_n}")
-#
-#
-#
-# # Находим кнопку "3л" и нажимаем её
-# search_button3 = browser.find_element(By.XPATH, '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[1]/div/div/div/section/div/ul/li[1]/div[2]/div/div/div[2]/div/a')
-# search_button3.click()
-#
-# # Найдем цену на 3 л
-# header_vacancies = browser.find_elements(By.XPATH, '//span[@class="sht-price"][1]')
-# header_name = header_vacancies[1].text
-#
-# # Форматируем строку с помощью регулярного выражения
-# vacancies_count = header_name
-# print(f"Цена за 3 л: {vacancies_count}")
-#
-#
-# # # Находим кнопку "10л" и нажимаем её
-# search_button10 = browser.find_element(By.XPATH, '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[1]/div/div/div/section/div/ul/li[1]/div[2]/div/div/div[3]/div/a')
-# search_button10.click()
-#
-# # Найдем цену на 10л
-# price = browser.find_element(By.XPATH, '/html/body/div[4]/div[1]/div[2]/div/div[1]/main/div[1]/div[2]/div[2]/aside/div/div[1]/table[1]/tbody/tr/td/span[1]')
-# price_n = price.text
-#
-# # Печатпем цену
-# print(f"Цена за 10 л: {price_n}")
-#
-#
-# # Ждем
-# time.sleep(2)
-#
-# # Закрываем браузер
-# #browser.close()
--- exel_1.py
+++ exel_1.py
@@ -1,13 +1,45 @@
 import openpyxl
+from openpyxl.utils import get_column_letter
+import re
 
-wb = openpyxl.reader.excel.load_workbook(filename="PRICE2022.xlsx", data_only=True)
+path_to_file = 'PRICE2022.xlsx'
 
-print(wb.sheetnames)
+# search_text = input(str('Какой текст ищем: '))
+# search_text = search_text.lower()
+search_text = '00-00009567'
+print('Ищем:', search_text)
 
-wb.active = 0
+wb = openpyxl.load_workbook(path_to_file)  # Грузим наш прайс-лист
+sheets_list = wb.sheetnames  # Получаем список всех листов в файле
+sheet_active = wb[sheets_list[0]]  # Начинаем работать с самым первым
+row_max = sheet_active.max_row  # Получаем количество столбцов
+# print(type(row_max))
+column_max = sheet_active.max_column  # Получаем количество строк
 
-sheet = wb.active
-print(sheet['A1'])
+print('В файле:', path_to_file, '\n Строк:', row_max, '\n Столбцов:', column_max)
 
-#for i in range(1,12):
-#    print(sheet['A'+str(i)].value,sheet['B'+str(i)].value,sheet['C'+str(i)].value)
+row_min = 1  # Переменная, отвечающая за номер строки
+column_min = 1  # Переменная, отвечающая за номер столбца
+
+while column_min <= column_max:
+    row_min_min = row_min
+    row_max_max = row_max
+    while row_min_min <= row_max_max:
+        row_min_min = str(row_min_min)
+
+        word_column = get_column_letter(column_min)
+        word_column = str(word_column)
+        word_cell = word_column + row_min_min
+
+        data_from_cell = sheet_active[word_cell].value
+        data_from_cell = str(data_from_cell)
+        # print(data_from_cell)
+        regular = search_text
+        result = re.findall(regular, data_from_cell)
+        if len(result) > 0:
+            print('Нашли в ячейке:', word_cell)
+            RRC='C' + row_min_min
+            print( 'РРЦ равняется ',sheet_active[RRC].value,' рубль' )
+        row_min_min = int(row_min_min)
+        row_min_min = row_min_min + 1
+    column_min = column_min + 1
