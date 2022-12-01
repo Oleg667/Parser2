82e298be86ac57085518937b4797546823762313
grishaev <oleg-g@yandex.ru>
2022-12-01 13:49:04 +0300

    Initial commit

--- check_xls.py
+++ check_xls.py
@@ -22,14 +22,19 @@ from openpyxl.utils import get_column_letter
 from tkinter import *
 from tkinter.messagebox import showinfo
 
-def list_parser():# Получаем список листов из файла эксель
+def list_parser():
 
     wb = openpyxl.reader.excel.load_workbook(filename="PRICE2022.xlsx", data_only=True)  # открываем файл с переменными
 
-    list_par = wb.sheetnames# список листов
-    del list_par[0] # Удалеям имя первого (нулевого) листа с контрольным прайсом
+    list_par = wb.sheetnames # список листов
 
     return (list_par)
+
+    # for sheet in wb: # печатаем список листов по одному
+    #     print(sheet.title)
+
+
+
 
 
 
@@ -44,30 +49,33 @@ root.iconphoto(False,photo)
 download_label = Label(root, text = 'Выберите что будем проверять', font=35) # текст в окне
 download_label.pack(pady = 20)
 
+def checkbutton_changed():
+
+    showinfo(title="Info",message=enabled.get())
 
 
-
-
-# создаем список
+# создаем чекбоксы
 
 list_par=list_parser() #обращаемся к функции для получения списка листов для парсинга
 
-#languages = ["Python", "JavaScript", "C#", "Java"]
-languages_var = Variable(value=list_par)
+enabled = StringVar()
+n=0
 
-languages_listbox = Listbox(listvariable=languages_var)
+for i in list_par:
+    if n != 0: # Пропускаем первый лист где находится контрольные цены
+        enabled_checkbutton = tkinter.Checkbutton(root, text=i, variable=enabled, offvalue="", onvalue=i, command=checkbutton_changed)
+        enabled_checkbutton.pack(padx=6, pady=6, anchor=NW)
+    n = +1
+#
+# prs2 = tkinter.Checkbutton(root, text=list_par[2])
+# prs2.pack()
+# prs3 = tkinter.Checkbutton(root, text=list_par[3])
+# prs3.pack()
 
-languages_listbox.pack(anchor=NW, fill=X, padx=5, pady=5)
 
 
-
-
-# Кнопка
-button = Button(root,text=" Запустить Парсер ", font = 40, command= list_parser) # создаем кнопку
+button = Button(root,text=" Запустить Парсер ", font = 40, command= list_parser) # кнопка
 button.pack(side = BOTTOM, pady = 40) # расположение кнопки, отступ по оси У
-
-
-
 
 root.mainloop() # запуск визуализации окна
 
